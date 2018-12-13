3d9562778c267fc4b605b57b15c234cae7f26ea5
Thomas Barham <Thomas.Barham@ukho.gov.uk>
2018-12-13 15:03:52 +0000

    added css to creator area

--- public/js/creator.js
+++ public/js/creator.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
     $("#add-question").click(function () { 
-        
-        $("#question-list ul").append("<li>Test</li>");
+        $("#question-list ol").append("<li class='question-item'>" + $("#question-input").val() + "<i class='material-icons'>play_arrow</i>" +
+        "<div class='answers-div'>" +
+        "<ol><li>" + $("#answer1-input").val() + "</li><hr>" + 
+        "<li>" + $("#answer2-input").val() + "</li><hr>" + 
+        "<li>" + $("#answer3-input").val() + "</li></ol></div></li>");
     });
+   
 });
